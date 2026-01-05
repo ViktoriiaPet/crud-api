87a1ce35de1bbb4f58034e8d48747f143ab6fe1f
Viktoriia Petukhova <viktoriiap0205199@gmail.com>
2026-01-05 16:28:58 +0100

    feat: add services for user

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,6 @@ export const app: Application = express();
 
 app.use(express.json())
 
-app.get('health', (req: Request, res: Response) => {
+app.get('/health', (req: Request, res: Response) => {
     res.status(200).json({status: 'ok'})
 })
---
+++ src/services/userService.ts
@@ -0,0 +1,29 @@
+import {type User } from "../models/User.js";
+let users: User[] = []
+
+export const getAllUser = ():User[] => {
+    return users
+}
+
+export const getUserById = (id:number) : User | undefined => {
+    return users.find(u => u.id === id)
+}
+
+export const createUser = (user:User) : User => {
+    users.push(user);
+    return user
+}
+
+export const updateUser = (id:number, updateUser: Partial<User>): User | undefined => {
+    const user = users.find(u => u.id === id);
+    if (!user) return undefined;
+    Object.assign(user, updateUser);
+    return user 
+}
+
+export const deleteUser = (id:number): boolean  => {
+    const index = users.findIndex (u => u.id === id)
+    if (index === -1) return false;
+    users.splice(index, 1)
+    return true
+}
