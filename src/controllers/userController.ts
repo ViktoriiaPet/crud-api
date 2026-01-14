84678ae28eb3ee61a1f6c3a7c99bee883b7a76f7
Viktoriia Petukhova <viktoriiap0205199@gmail.com>
2026-01-14 09:32:26 +0100

    fix: add missing await in user controllers

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,35 +2,35 @@
  import * as userservice from "../services/userService.js"
  import { type User } from '../models/User.js';
 
-export const getUsers = (req: Request, res: Response) => {
-    const users = userservice.getAllUser();
+export const getUsers = async (req: Request, res: Response) => {
+    const users = await userservice.getAllUser();
     res.json(users)
 }
 
-export const getUser = (req: Request, res: Response) => {
+export const getUser = async (req: Request, res: Response) => {
     const id = Number(req.params.id)
-    const user = userservice.getUserById(id);
+    const user = await userservice.getUserById(id);
     if (!user) return res.status(400).json({message: 'User is not found'})
     return res.json(user)
 }
 
-export const createUser = (req: Request, res: Response) => {
+export const createUser = async (req: Request, res: Response) => {
     const {name, email} = req.body;
     const user: User = { id: Date.now(), name, email };
-    const newUser = userservice.createUser(user)
+    const newUser = await userservice.createUser(user)
     res.status(201).json(newUser)
 }
 
-export const updateUser = (req: Request, res: Response) => {
+export const updateUser = async (req: Request, res: Response) => {
     const id = Number(req.params.id);
-    const updateUser = userservice.updateUser(id, req.body)
+    const updateUser = await userservice.updateUser(id, req.body)
     if (!updateUser) return res.status(404).json({message: 'User is not found'})
     res.json (updateUser)
 }
 
-export const deleteUser = (req: Request, res: Response) => {
+export const deleteUser = async (req: Request, res: Response) => {
     const id = Number(req.params.id)
-    const succes = userservice.deleteUser(id)
+    const succes = await userservice.deleteUser(id)
     if (!succes) res.status(404).json({message: "user not found"})
     res.json(succes)
 }
