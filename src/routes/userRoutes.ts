6adbca8a309eeb900fe77772c77b070951f4040d
Viktoriia Petukhova <viktoriiap0205199@gmail.com>
2026-01-14 09:35:20 +0100

    fix: remove validation from GET endpoint

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -5,7 +5,7 @@ import { validateUser } from "../middlewares/validateUser.js";
 export const userRouter = Router()
 
 userRouter.get("/", userController.getUsers)
-userRouter.get("/:id", validateUser, userController.getUser)
+userRouter.get("/:id", userController.getUser)
 userRouter.post("/", validateUser, userController.createUser)
 userRouter.put("/:id", validateUser, userController.updateUser)
 userRouter.delete("/:id", validateUser, userController.deleteUser)
