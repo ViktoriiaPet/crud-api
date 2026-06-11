9418617075bdbbb9fd4c78675732f038f54242d6
Viktoriia Petukhova <viktoriiap0205199@gmail.com>
2026-06-11 20:07:14 +0200

    fix: update data base

--- src/docs/crud_api_schema.sql
+++ src/docs/crud_api_schema.sql
@@ -2,17 +2,19 @@
 
 -- DROP TABLE IF EXISTS public.users;
 
-CREATE TABLE IF NOT EXISTS public.users
-(
-    id integer NOT NULL DEFAULT nextval('users_id_seq'::regclass),
-    name character varying(100)[] COLLATE pg_catalog."default" NOT NULL,
-    email character varying(100)[] COLLATE pg_catalog."default" NOT NULL,
-    created_at timestamp with time zone DEFAULT now(),
-    updated_at time with time zone DEFAULT now(),
-    CONSTRAINT users_pkey PRIMARY KEY (id, email)
-)
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  email VARCHAR(150) UNIQUE NOT NULL,
+  password TEXT NOT NULL,
+  role VARCHAR(20) DEFAULT 'user',
+  created_at TIMESTAMPTZ DEFAULT NOW(),
+  updated_at TIMESTAMPTZ DEFAULT NOW()
+);
 
 TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS public.users
     OWNER to postgres;
+
+    
