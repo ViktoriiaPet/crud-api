dd13a6716458fb321dcd2b84e43fcc5af5204bcc
Viktoriia Petukhova <viktoriiap0205199@gmail.com>
2026-06-14 10:22:20 +0200

    fix: update  role middleware: delete useless elements

--- src/middlewares/roleMiddleware.ts
+++ src/middlewares/roleMiddleware.ts
@@ -1,10 +1,5 @@
 import type { Request, Response, NextFunction } from "express";
 
-interface TokenPayload  {
-    "id": number,
-    "role": string
-}
-
 export const roleMiddleware = (req: Request, res: Response, next: NextFunction) => {
 
     if (req.user?.role === "admin") {
@@ -12,7 +7,7 @@ export const roleMiddleware = (req: Request, res: Response, next: NextFunction) 
     } else if (!req.user) {
         res.status(401).json({message: "You are not logged in"})
     } else {
-        res.status(403).json({message: "access denied"})
+        res.status(403).json({message: "Access denied"})
     }
 
 }
